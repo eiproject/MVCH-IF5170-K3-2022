0fa041b3e8517f9057a9ea2c7e8c9e471616eb1e
M Razif Rizqullah <razifrizqullah@gmail.com>
2022-12-17 13:31:58 +0700

    doctor and nurse ok

--- core/views_dashboard.py
+++ core/views_dashboard.py
@@ -17,37 +17,59 @@ def check_jwt(sess) -> Tuple[str, str]:
         else:
             return redirect('/logout')
             
-            
+
 @app.route("/dashboard", methods=["GET"])
 def dashboard():
     email, user_type = check_jwt(session)
+    user_type = UserType.NURSE # override for dev
     if user_type == UserType.PATIENT:
-        return render_template('dashboard/patient-home.html', Name="Dashboard", EMAIL=email)
+        return render_template('dashboard/patient-home.html', Name="Dashboard", EMAIL=email, USER_TYPE=user_type)
     elif user_type == UserType.DOCTOR:
-        return render_template('dashboard/doctor-home.html', Name="Dashboard", EMAIL=email)
+        return render_template('dashboard/doctor-home.html', Name="Dashboard", EMAIL=email, USER_TYPE=user_type)
+    elif user_type == UserType.NURSE:
+        return render_template('dashboard/nurse-home.html', Name="Dashboard", EMAIL=email, USER_TYPE=user_type)
 
 
 @app.route("/dashboard/patient-registration", methods=["GET"])
 def patient_registration():
     email, user_type = check_jwt(session)
-    return render_template('dashboard/patient-registration.html', Name="Patient Registration", EMAIL=email)
+    return render_template('dashboard/patient-registration.html', Name="Patient Registration", EMAIL=email, USER_TYPE=user_type)
 
 
 @app.route("/dashboard/register-consultation", methods=["GET"])
 def register_consultation():
     email, user_type = check_jwt(session)
-    return render_template('dashboard/patient-register-consultation.html', Name="Register Consultation", EMAIL=email)
+    return render_template('dashboard/patient-register-consultation.html', Name="Register Consultation", EMAIL=email, USER_TYPE=user_type)
 
     
 @app.route("/dashboard/history-consultation", methods=["GET"])
 def history_consultation():
     email, user_type = check_jwt(session)
-    return render_template('dashboard/patient-history-consultation.html', Name="History Consultation", EMAIL=email)
+    user_type = UserType.NURSE # override for dev
+    return render_template('dashboard/history-consultation.html', Name="History Consultation", EMAIL=email, USER_TYPE=user_type)
 
 
 @app.route("/dashboard/doctor-schedule", methods=["GET"])
 def doctor_schedule():
     email, user_type = check_jwt(session)
-    return render_template('dashboard/patient-doctor-schedule.html', Name="Doctor Schedule", EMAIL=email)
+    user_type = UserType.DOCTOR # override for dev
+    return render_template('dashboard/patient-doctor-schedule.html', Name="Doctor Schedule", EMAIL=email, USER_TYPE=user_type)
+
+@app.route("/dashboard/consultation-schedule", methods=["GET"])
+def consultation_schedule():
+    email, user_type = check_jwt(session)
+    user_type = UserType.DOCTOR # override for dev
+    return render_template('dashboard/doctor-consultation-schedule.html', Name="Consultation Schedule", EMAIL=email, USER_TYPE=user_type)
 
     
+@app.route("/dashboard/patient-list", methods=["GET"])
+def patient_list():
+    email, user_type = check_jwt(session)
+    user_type = UserType.DOCTOR # override for dev
+    return render_template('dashboard/doctor-patient-list.html', Name="Patient List", EMAIL=email, USER_TYPE=user_type)
+
+@app.route("/dashboard/nurse-schedule", methods=["GET"])
+def nurse_schedule():
+    email, user_type = check_jwt(session)
+    user_type = UserType.NURSE # override for dev
+    return render_template('dashboard/nurse-schedule.html', Name="Nurse Schedule", EMAIL=email, USER_TYPE=user_type)
