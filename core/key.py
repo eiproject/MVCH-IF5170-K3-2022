862086d8e5f3c328a6c6b60250cc57132862fd51
M Razif Rizqullah <razifrizqullah@gmail.com>
2022-12-18 15:25:31 +0700

    render doctor schedule ok

--- core/key.py
+++ core/key.py
@@ -1,8 +1,46 @@
-def CreateUserKey(hospital_id, user_id):
+def CreateUserKey(hospital_id:str, user_id:str) -> str:
     return f'{hospital_id}:User[id:{user_id}]'
 
-def GetUserIdFromKey(user_key:str):
+def GetUserIdFromKey(user_key:str) -> str:
     return user_key.split('id:')[-1].replace(']','')
 
-def CreatePatientKey(hospital_id, user_id):
+def CreatePatientKey(hospital_id:str, user_id:str) -> str:
     return f'{hospital_id}:Patient[id:{user_id}]'
+
+def CreateScheduleKey(hospital_id:str, schedule_id:int) -> str:
+    # Hospital_id:Schedule[id:schedule_id] = start timestamp end timestamp
+    return f'{hospital_id}:Schedule[id:{schedule_id}]'
+
+def CreatePhysicianScheduleKey(hospital_id:str, user_id:str) -> str:
+    # Hospital_id:PhysicianSchedule[id:user_id] = [ schedule_id ]
+    return f'{hospital_id}:PhysicianSchedule[id:{user_id}]'
+
+# dummy data generator 
+def generate_dummy_schedule(db, hospital_id):
+    id = 0
+    for day in range(19, 24):
+        for hour in range(7, 17):
+            key = CreateScheduleKey(hospital_id, id)
+            start = f'2022-12-{day}T{hour}:00:00'
+            end = f'2022-12-{day}T{hour}:59:59'
+            if not db.hgetall(key):
+                db.hset(
+                    name=key, 
+                    mapping={
+                        'start': start,
+                        'end': end,
+                    }
+                )
+            id+=1
+
+def generate_dummy_phy_schedule(db, hospital_id, email):
+    import random 
+    random.seed(1)
+    schedules = random.choices(list(range(0, 50)), k=10)
+    key = CreatePhysicianScheduleKey(hospital_id, email)
+    for s in schedules:
+        db.sadd(key, s)
+
+
+# generate_dummy_schedule(db, hospital_id)
+# generate_dummy_phy_schedule(db, hospital_id, 'doctor@gmail.com')
--- core/views_dashboard.py
+++ core/views_dashboard.py
@@ -1,11 +1,11 @@
 from typing import Tuple
 from core.entity import UserType
-from core.key import CreateUserKey, GetUserIdFromKey, CreatePatientKey
+from core.key import CreatePatientKey, CreatePhysicianScheduleKey, CreateScheduleKey, generate_dummy_schedule
 from core.util import check_jwt, get_session_key
 from . import app, jwt, db, hospital_id
 from flask import Flask, request, render_template, redirect, jsonify, session
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, decode_token
-
+from datetime import datetime, timedelta
 
 @app.route("/dashboard", methods=["GET"])
 def dashboard():
@@ -18,7 +18,7 @@ def dashboard():
         template = 'dashboard/doctor-home.html'
     elif user_type == UserType.NURSE:
         template = 'dashboard/nurse-home.html'
-
+    
     return render_template(template, 
         Name="Dashboard", 
         EMAIL=email, 
@@ -75,12 +75,74 @@ def history_consultation():
 def doctor_schedule():
     email, user_type = check_jwt(db, session)
     if email is None: return redirect('/logout')
+
+    now = datetime.now()
+    thresh = now + timedelta(days=7)
+
+    key = CreatePhysicianScheduleKey(hospital_id, email)
+    schedules_id = [int(v) for v in db.smembers(key)]
+    schedules_id.sort()
+    # timeslot = {
+    #     'date': [ hour ]
+    # }
+    timeslot = {}
+
+    for id in schedules_id:
+        sch_key = CreateScheduleKey(hospital_id, id)
+        sch_data = db.hgetall(sch_key)
+        start = sch_data[b'start'].decode("utf-8")
+        end = sch_data[b'end'].decode("utf-8")
+
+        start_date = datetime.strptime(start, "%Y-%m-%dT%H:%M:%S")
+        
+        date_key = start_date.strftime("%m/%d %A")
+        # day = start_date.strftime('%A')
+        hour = start_date.hour
+
+        if start_date.date() >= now.date() and  start_date.date() < thresh.date():
+            if date_key in timeslot:
+                timeslot[date_key].append(hour)
+            else:
+                timeslot[date_key] = [hour]
+
+    print(timeslot)
+    
+    # rendering 
+    header = ['Time']
+    for i in range(7):
+        t = now + timedelta(days=i)
+        d = t.strftime("%m/%d %A")
+        header.append(d)
+
+    timeslot_render = [header]
+
+    for hour in range(7, 19):
+        row = [f'{hour}:00']
+        for d_key in header[1:]:
+            if d_key in timeslot:
+                if hour in timeslot[d_key]:
+                    row.append(True)
+                else:
+                    row.append(False)
+            else:
+                row.append(False)
+        timeslot_render.append(row)
+
+    print(timeslot_render)
+
+
+
     return render_template(
         'dashboard/patient-doctor-schedule.html', 
         Name="Doctor Schedule", 
         EMAIL=email, 
         USER_TYPE=user_type, 
         USER_FULLNAME=email,
+        USER_NAME=email.split('@')[0],
+        USER_SPECIALIZATION='Dentist',
+        TIMESLOT_HEADER=timeslot_render[0],
+        TIMESLOT_ITEM=timeslot_render[1:],
+        FROM_TO_SCHEDULE_RANGE=f'from {header[1]} to {header[-1]}',
         )
 
 @app.route("/dashboard/consultation-schedule", methods=["GET"])
